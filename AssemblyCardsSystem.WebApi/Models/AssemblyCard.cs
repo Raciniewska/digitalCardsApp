48d7cd11448d3e4fec869c75bdd222d63d49af2d
Raciniewska <raciniewska.barbara@gmail.com>
2020-10-10 11:47:43 +0200

    widoki admin

---
+++ AssemblyCardsSystem.WebApi/Models/AssemblyCard.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AssemblyCardsSystem.WebApi.Models
+{
+    public class AssemblyCard
+    {
+        public string EmployeeLN { get; set; }
+        public string EmployeeFN { get; set; }
+        public string EmployeeID { get; set; }
+        private DateTime CreationDate { get; set; }
+        public string KNNR { get; set; }
+        public string Sort { get; set; }
+        public string PrNr { get; set; }
+    }
+}
